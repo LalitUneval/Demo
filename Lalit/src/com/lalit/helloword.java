3b5b6b0ffd084b890e4d883c112cca0a59175946
LalitUneval <unevallalit6499@gmail.com>
2025-03-31 10:15:54 +0530

    third commit

---
+++ Lalit/src/com/lalit/helloword.java
@@ -0,0 +1,8 @@
+package com.lalit;
+
+public class helloword {
+
+	public static void main(String[] args) {
+		System.out.println("hello how are you!!");
+	}
+}
--- Lalit/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Lalit {
-	hello how are you
-}
